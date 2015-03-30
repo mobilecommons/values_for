e3bb6ce4e82dcf69859f4c72264231d8358cf41c
Mal McKay <mal.mckay@gmail.com>
2015-03-29 23:46:07 -0400

    BUGFIX Rails 4 requires callable scopes

--- lib/values_for.rb
+++ lib/values_for.rb
@@ -50,9 +50,9 @@ module ValuesFor
 
         # Create +optional+ named scopes, Rails <= 2.3.x
         named_scope prefixed_val, :conditions => { attribute => val_s } if additives.include?(:named_scopes)
-        
+
         # Create +optional+ scopes, Rails >= 3.0.x
-        scope prefixed_val, where(attribute => val_s) if additives.include?(:scopes)
+        scope prefixed_val, lambda{ where(attribute => val_s) } if additives.include?(:scopes)
 
         # Create +optional+ predicate methods, but don't overwrite existing methods
         if additives.include?(:predicate_methods)
